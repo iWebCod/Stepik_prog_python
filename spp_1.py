28a983ec9c69f44b6f5065dd9554fbd59494aedf
iWebCod <iwebcod@gmail.com>
2021-09-08 12:37:21 +0300

    git config --global user.email "iwebcod@gmail.com" git config --global user.name "iWebCod"

--- spp_1.py
+++ spp_1.py
@@ -1,21 +1,40 @@
-print(2+5)
+# print(2+5)
+#
+# print(11111 * 1111111)
+#
+# print (40//8)
+#
+# print (42//8)
+#
+# #print (42// (4 + 2 * (-2)))
+#
+# print(2014**14)
+#
+# print(1.2345e-3)
+#
+# print(2014.0**14)
+#
+# print(7/3)
+#
+# print(7//3)
+#
+# #https://github.com/snfrolov/Python-course-stepik.org-/blob/master/1.-Operators.-Variables.-Data-types.-Conditions/1.6
+#
+# print(int(-1.6))
+#
+# #print(9**19 - int(float(9**19)))
 
-print(11111 * 1111111)
+# a = (9**19)
+# b = (int(float(9**19)))
+# с = (a - b)
+# print (с)
+# с +=1
+# print("Итого:",с)
 
-print (40//8)
+name = input('Enter your name: ')
+print('Привет,', name)
+date_day = input('Введите дату вашего рождения: ')
+date_month = input('Введите месяц вашего рождения прописью: ')
+date_year = input('Введите год вашего рождения: ')
+print('Привет,', name, 'вы родились:',date_day,date_month,date_year,'Все верно?')
 
-print (42//8)
-
-#print (42// (4 + 2 * (-2)))
-
-print(2014**14)
-
-print(1.2345e-3)
-
-print(2014.0**14)
-
-print(7/3)
-
-print(7//3)
-
-#https://github.com/snfrolov/Python-course-stepik.org-/blob/master/1.-Operators.-Variables.-Data-types.-Conditions/1.6
