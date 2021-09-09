07256f22c16fddc8807490ea9c4f0449dbb1024f
iWebCod <iwebcod@gmail.com>
2021-09-09 13:57:18 +0300

    git config --global user.email "iwebcod@gmail.com" git config --global user.name "iWebCod"

--- spp_1.py
+++ spp_1.py
@@ -52,3 +52,15 @@
 # m = int(input())
 # print((60*h+m+x) // 60)
 # print((60*h+m+x) % 60)
+
+# a = int(input())
+# print (a >= 10 and a <=100)
+# print (10 <= a < 100)
+
+# x = 5
+# y = 10
+# print (y > x * x or y >= 2 * x and x < y)
+
+# a = True
+# b = False
+# print (a and b or not a and not b)
